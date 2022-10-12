37cd95593b9f5b0420841f0f7ed87ec13b5b7f70
John Wynne <wynnej1@gamatc.matc.edu>
2022-10-11 21:29:50 -0500

    p1 fixes

--- blog/gatsby-config.js
+++ blog/gatsby-config.js
@@ -4,7 +4,7 @@ require("dotenv").config({
 
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Blog`,
+    title: `Cyberdyne Systems`,
     description: `ITDEV-164 Gatsby Blog`,
     author: `John Wynne`,
     contact: {
--- blog/src/pages/about.js
+++ blog/src/pages/about.js
@@ -5,23 +5,21 @@ import { StaticImage } from "gatsby-plugin-image"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
-
-
 const About = ({ data }) => {
-    const { name, company} = data.site.setMetadata.contact;
-    return (
-      <Layout>
-        <SEO title="About" />
-        <h1>About Us</h1>
-        <p>{`${company} was started by ${name} in 2022`}</p>
-        <p>{`At ${company} we just make blargs!`}</p>
+  const { name, company } = data.site.setMetadata.contact
+  return (
+    <Layout>
+      <SEO title="About" />
+      <h1>About Us</h1>
+      <p>{`${company} was started by ${name} in 2022`}</p>
+      <p>{`At ${company} we just make blargs!`}</p>
 
-        <div style={{maxWidth:`300px`, marginBottom:`1.45rem`}}>
+      <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
         <Image />
-        </div>
-        <Link to="/">Home</Link>
-      </Layout>
-    )
+      </div>
+      <Link to="/">Home</Link>
+    </Layout>
+  )
 }
 
 export default About
